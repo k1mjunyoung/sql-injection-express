7ac2a31f044278e6d8abd56aa5bdf17677698bd9
GeonwooShin <rjsdnslawkd@naver.com>
2023-03-29 17:41:41 +0900

    feat : 페이지 퍼블리싱

--- lib_login/auth.js
+++ lib_login/auth.js
@@ -1,13 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var template = require('./template.js');
-var db = require('./db');
+var template = require("./template.js");
+var db = require("./db");
 
 // 로그인 화면
-router.get('/login', function (request, response) {
-    var title = '로그인';
-    var html = template.HTML(title,`
+router.get("/login", function (request, response) {
+  var title = "로그인";
+  var html = template.HTML(
+    title,
+    `
             <h2>로그인</h2>
             <form action="/auth/login_process" method="post">
             <p><input class="login" type="text" name="username" placeholder="아이디"></p>
@@ -15,89 +17,126 @@ router.get('/login', function (request, response) {
             <p><input class="btn" type="submit" value="로그인"></p>
             </form>            
             <p>계정이 없으신가요?  <a href="/auth/register">회원가입</a></p> 
-        `, '');
-    response.send(html);
+        `,
+    ""
+  );
+  response.send(html);
 });
 
 // 로그인 프로세스
-router.post('/login_process', function (request, response) {
-    var username = request.body.username;
-    var password = request.body.pwd;
-    if (username && password) {             // id와 pw가 입력되었는지 확인
-        
-        db.query(`SELECT * FROM user WHERE username = '${username}' AND password = '${password}'`, function(error, results, fields) {
-            if (error) throw error;
-            if (results.length > 0) {       // db에서의 반환값이 있으면 로그인 성공
-                request.session.is_logined = true;      // 세션 정보 갱신
-                request.session.nickname = username;
-                request.session.save(function () {
-                    response.redirect(`/`);
-                });
-            } else {              
-                response.send(`<script type="text/javascript">alert("로그인 정보가 일치하지 않습니다."); 
-                document.location.href="/auth/login";</script>`);    
-            }            
-        });
+router.post("/login_process", function (request, response) {
+  var username = request.body.username;
+  var password = request.body.pwd;
+  if (username && password) {
+    // id와 pw가 입력되었는지 확인
 
-    } else {
-        response.send(`<script type="text/javascript">alert("아이디와 비밀번호를 입력하세요!"); 
-        document.location.href="/auth/login";</script>`);    
-    }
+    db.query(
+      `SELECT * FROM user WHERE username = '${username}' AND password = '${password}'`,
+      function (error, results, fields) {
+        if (error) throw error;
+        if (results.length > 0) {
+          // db에서의 반환값이 있으면 로그인 성공
+          request.session.is_logined = true; // 세션 정보 갱신
+          request.session.nickname = username;
+          request.session.save(function () {
+            response.redirect(`/`);
+          });
+        } else {
+          response.send(`<script type="text/javascript">alert("로그인 정보가 일치하지 않습니다."); 
+                document.location.href="/auth/login";</script>`);
+        }
+      }
+    );
+  } else {
+    response.send(`<script type="text/javascript">alert("아이디와 비밀번호를 입력하세요!"); 
+        document.location.href="/auth/login";</script>`);
+  }
 });
 // 로그아웃
-router.get('/logout', function (request, response) {
-    request.session.destroy(function (err) {
-        response.redirect('/');
-    });
+router.get("/logout", function (request, response) {
+  request.session.destroy(function (err) {
+    response.redirect("/");
+  });
 });
-
 
 // 회원가입 화면
-router.get('/register', function(request, response) {
-    var title = '회원가입';    
-    var html = template.HTML(title, `
-    <h2>회원가입</h2>
-    <form action="/auth/register_process" method="post">
-    <p><input class="login" type="text" name="username" placeholder="아이디"></p>
-    <p><input class="login" type="password" name="pwd" placeholder="비밀번호"></p>    
-    <p><input class="login" type="password" name="pwd2" placeholder="비밀번호 재확인"></p>
-    <p><input class="btn" type="submit" value="제출"></p>
-    </form>            
-    <p><a href="/auth/login">로그인화면으로 돌아가기</a></p>
-    `, '');
-    response.send(html);
+router.get("/register", function (request, response) {
+  var title = "회원가입";
+  var html = template.HTML(
+    title,
+    `
+    <div class="wrapper">
+      <div class="container">
+        <div class="sign-in-container">
+          <form action="/auth/register_process" method="post">
+            <h1>회원가입</h1>
+            <input type="text" name="username" placeholder="아이디" />
+            <input type="password" name="pwd" placeholder="비밀번호" />
+            <input type="password" name="pwd2" placeholder="비밀번호 재확인" />
+            <button class="form_btn" type="submit">제출</button>
+          </form>
+        </div>
+        <div class="overlay-container">
+          <div class="overlay-left">
+            <h1>Welcome Back</h1>
+            <p>
+              To keep connected with us please login with your personal info
+            </p>
+            <button id="signIn" class="overlay_btn">Sign In</button>
+          </div>
+          <div class="overlay-right">
+            <h1>SQL Injection</h1>
+            <button id="signUp" class="overlay_btn">회원가입</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `,
+    ""
+  );
+  response.send(html);
 });
- 
+
 // 회원가입 프로세스
-router.post('/register_process', function(request, response) {    
-    var username = request.body.username;
-    var password = request.body.pwd;    
-    var password2 = request.body.pwd2;
+router.post("/register_process", function (request, response) {
+  var username = request.body.username;
+  var password = request.body.pwd;
+  var password2 = request.body.pwd2;
 
-    if (username && password && password2) {
-        
-        db.query('SELECT * FROM user WHERE username = ?', [username], function(error, results, fields) { // DB에 같은 이름의 회원아이디가 있는지 확인
-            if (error) throw error;
-            if (results.length <= 0 && password == password2) {     // DB에 같은 이름의 회원아이디가 없고, 비밀번호가 올바르게 입력된 경우 
-                db.query('INSERT INTO user (username, password) VALUES(?,?)', [username, password], function (error, data) {
-                    if (error) throw error2;
-                    response.send(`<script type="text/javascript">alert("회원가입이 완료되었습니다!");
+  if (username && password && password2) {
+    db.query(
+      "SELECT * FROM user WHERE username = ?",
+      [username],
+      function (error, results, fields) {
+        // DB에 같은 이름의 회원아이디가 있는지 확인
+        if (error) throw error;
+        if (results.length <= 0 && password == password2) {
+          // DB에 같은 이름의 회원아이디가 없고, 비밀번호가 올바르게 입력된 경우
+          db.query(
+            "INSERT INTO user (username, password) VALUES(?,?)",
+            [username, password],
+            function (error, data) {
+              if (error) throw error2;
+              response.send(`<script type="text/javascript">alert("회원가입이 완료되었습니다!");
                     document.location.href="/";</script>`);
-                });
-            } else if (password != password2) {                     // 비밀번호가 올바르게 입력되지 않은 경우
-                response.send(`<script type="text/javascript">alert("입력된 비밀번호가 서로 다릅니다."); 
-                document.location.href="/auth/register";</script>`);    
             }
-            else {                                                  // DB에 같은 이름의 회원아이디가 있는 경우
-                response.send(`<script type="text/javascript">alert("이미 존재하는 아이디 입니다."); 
-                document.location.href="/auth/register";</script>`);    
-            }            
-        });
-
-    } else {        // 입력되지 않은 정보가 있는 경우
-        response.send(`<script type="text/javascript">alert("입력되지 않은 정보가 있습니다."); 
+          );
+        } else if (password != password2) {
+          // 비밀번호가 올바르게 입력되지 않은 경우
+          response.send(`<script type="text/javascript">alert("입력된 비밀번호가 서로 다릅니다."); 
+                document.location.href="/auth/register";</script>`);
+        } else {
+          // DB에 같은 이름의 회원아이디가 있는 경우
+          response.send(`<script type="text/javascript">alert("이미 존재하는 아이디 입니다."); 
+                document.location.href="/auth/register";</script>`);
+        }
+      }
+    );
+  } else {
+    // 입력되지 않은 정보가 있는 경우
+    response.send(`<script type="text/javascript">alert("입력되지 않은 정보가 있습니다."); 
         document.location.href="/auth/register";</script>`);
-    }
+  }
 });
 
 module.exports = router;
